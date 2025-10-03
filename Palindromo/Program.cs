578d5a012f3657733a962e18f2e365d290e83349
Olivio Barreto <oliviobarreto49@gmail.com>
2025-10-03 14:13:48 -0300

    Adicionando Projeto Palindromo

---
+++ Palindromo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace LeetCode.Palindromo;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    var result = IsPalindromo(101);
+    Console.WriteLine(result);
+  }
+
+  static bool IsPalindromo(int x)
+  {
+    string value = x.ToString();
+    StringBuilder palindromo = new();
+    for (int i = value.Length - 1; i >= 0; i--)
+    {
+      palindromo.Append(value[i].ToString());
+    }
+    return palindromo.Equals(value);
+  }
+}
