1fad020ddc6d86edfba289e7cacbd5f053f205ac
Olivio Barreto <oliviobarreto49@gmail.com>
2025-10-05 01:03:27 -0300

    Adicionando Desafio RomanoToInt

---
+++ RomanoToInt/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RomanoToInt;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    var result = RomanoToInt("IV");
+    Console.WriteLine(result);
+  }
+
+  static int RomanoToInt(string value)
+  {
+    var dict = new Dictionary<char, int>()
+    {
+      {'I', 1},
+      {'V', 5},
+      {'X', 10},
+      {'L', 50},
+      {'C', 100},
+      {'D', 500},
+      {'M', 1000}
+    };
+
+    int valorFinal = 0;
+
+    for (int i = 0; i < value.Length; i++)
+    {
+      int currentValue = dict[value[i]];
+
+      if (i + 1 < value.Length && currentValue < dict[value[i + 1]])
+        valorFinal -= dict[value[i]];
+      else
+        valorFinal += dict[value[i]];
+    }
+    return valorFinal;
+  }
+}
